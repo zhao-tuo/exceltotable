7098bdf85f0273cccc86fdcf38d21812be608e50
zhaotuo <zhao_tuo@126.com>
2018-09-14 13:47:38 +0800

    add excel to table processor

--- src/main/java/com/longfor/biz/CalculateDllProcessor.java
+++ src/main/java/com/longfor/biz/CalculateDllProcessor.java
@@ -1,25 +1,72 @@
 package com.longfor.biz;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 
 public class CalculateDllProcessor {
     public static void main(String[] args){
         System.out.println("hello world!");
-        String fileName="D:/test.csv";
-        String sql =  CalculateDllProcessor.generateSql(fileName);
-        System.out.println(sql);
-    }
-
-    public static String generateSql(String fileName){
-        File file = new File(fileName);
-        BufferedReader reader = null;
-        try{
-            reader = new BufferedReader(new FileReader(file));
+        String fileName="D:/test1.csv";
+        String sql = "";
+        try {
+            sql =  CalculateDllProcessor.generateSql(fileName);
         }catch (Exception ex){
             ex.printStackTrace();
         }
-        return null;
+        System.out.println(sql);
     }
+
+    public static String generateSql(String fileName) throws Exception{
+        File file = new File(fileName);
+        FileInputStream fis = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(fis,"GBK");
+        BufferedReader reader = null;
+        StringBuffer sql = new StringBuffer();
+        try{
+            reader = new BufferedReader(isr);
+            String lineStr = null;
+            int lineNum = 1;
+            while((lineStr=reader.readLine())!=null){
+               // System.out.println("line:" + lineNum + ",content:" + lineStr);
+                if(lineStr.startsWith("table-")){
+                    if(lineNum!=1){
+                        sql.deleteCharAt(sql.length()-1);
+                        sql.append(");\r\n");
+                    }
+                    String tableLine = lineStr.substring(6);
+                    String[] tableLineArr = tableLine.split("\\|");
+                    String tableName = tableLineArr[0].trim();
+                    sql.append("CREATE TABLE IF NOT EXISTS `"+ tableName+ "` (");
+                    continue;
+                }
+                String[] dataLineArr = lineStr.split("\\|");
+                if(dataLineArr.length>=4 && dataLineArr[3].equals("01")){
+                    String columnName = dataLineArr[0].trim();
+                    sql.append("`" + columnName + "` " + dataLineArr[2]+ " COMMENT '" + dataLineArr[1] + "',");
+                    sql.append("PRIMARY KEY (`" + columnName + "`),");
+                }else{
+                    String columnName = dataLineArr[0].trim();
+                    sql.append("`" + columnName + "` " + dataLineArr[2]+ " COMMENT '" + dataLineArr[1] + "',");
+                }
+                lineNum++;
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }finally {
+            if(reader!=null){
+                try {
+                    reader.close();
+                    isr.close();
+                    fis.close();
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+
+            }
+        }
+        return sql.toString();
+    }
+
+
+
+
 }
